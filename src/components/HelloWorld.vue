a8b21fc1db1674330cd0bd589f2d775528b61226
wangladi <wangladi@360.cn>
2021-12-31 12:22:27 +0800

    ADD: 引入ant-design-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,25 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <a-button type="primary">Primary Button</a-button>
+  <CloudServerOutlined />
+  <a-switch v-model:checked="checked" />
 </template>
 
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  export default defineComponent({
+    setup() {
+      const checked = ref<boolean>(false);
+      return {
+        checked,
+      };
+    },
+  });
+</script>
+
+
 <style scoped>
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
